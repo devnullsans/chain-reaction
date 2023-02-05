6294b471f160f0e07304a18d05991594b549bf44
Paul S <dedsecdarknet@gmail.com>
2023-02-05 12:51:13 +0530

    create game complete

--- handlers/create.js
+++ handlers/create.js
@@ -1,11 +1,15 @@
-import { rooms } from "../lib/games";
+import { rooms, Game } from "../lib/games";
 
 export async function create(req, res) {
   const { key } = req.user;
+  const { players, visible } = req.body;
 
   if (rooms.has(key)) return res.status(400).json({ error: "Room already exists." });
+  const totalPlayer = parseInt(players);
 
   // add new game to db for info
+  const game = new Game(key, totalPlayer, Boolean(visible));
+  rooms.set(key, game);
 
-  res.status(200).json({ data: "Farewell" });
+  res.status(200).json({ data: key });
 }
