10ce5f22db1f6bda3691c076fc88bbbcfb862fbd
DevNullSans <dedsecdarknet@gmail.com>
2023-02-06 18:38:58 +0530

    ws upgrade cookies receivable

--- client/src/comps/CreateGame.tsx
+++ client/src/comps/CreateGame.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function CreateGame({ reset = () => undefined as void }) {
-  const [players, setPlayers] = useState("2");
+  const [players, setPlayers] = useState('2');
   const [visible, setVisible] = useState(false);
 
   return (
@@ -9,6 +9,11 @@ export default function CreateGame({ reset = () => undefined as void }) {
       onSubmit={(e) => {
         e.preventDefault();
         // fetch api to create a new game for this user
+        const ws = new WebSocket(`${process.env.REACT_APP_SOK}/api/join`);
+        ws.onopen = (e) => console.log('websocker oppened', e);
+        ws.onclose = (e) => console.log('websocker closed', e);
+        ws.onerror = (e) => console.log('websocker error', e);
+        ws.onmessage = (e) => console.log('websocker message', e);
       }}>
       <select value={players} onChange={(e) => setPlayers(e.target.value)} required>
         <option value="2">2 Player</option>
@@ -19,12 +24,7 @@ export default function CreateGame({ reset = () => undefined as void }) {
         <option value="7">7 Player</option>
         <option value="8">8 Player</option>
       </select>
-      <input
-        type="checkbox"
-        checked={visible}
-        onChange={(e) => setVisible(e.target.checked)}
-        required
-      />
+      <input type="checkbox" checked={visible} onChange={(e) => setVisible(e.target.checked)} />
       Public ? <button>Create</button>
       <button type="button" onClick={reset}>
         Cancel
